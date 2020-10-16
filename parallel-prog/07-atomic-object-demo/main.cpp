bf710d8afbb7e964ded07330ba2ae88877c0c71e
Vinoth Kumar <vinothg.cdm@gmail.com>
2020-10-16 11:54:53 +0800

    Atomic object demo

---
+++ parallel-prog/07-atomic-object-demo/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Two shoppers adding items to a shared notepad
+ */
+#include <thread>
+#include <atomic>
+#include <stdio.h>
+
+std::atomic<uint32_t> garlic_count(0);
+
+void shopper()
+{
+    for (int i = 0; i < 10000000; i++) {
+        garlic_count++;
+    }
+}
+
+int main()
+{
+    std::thread barron(shopper);
+    std::thread olivia(shopper);
+    barron.join();
+    olivia.join();
+    printf("We should buy %u garlic.\n", garlic_count.load());
+}
