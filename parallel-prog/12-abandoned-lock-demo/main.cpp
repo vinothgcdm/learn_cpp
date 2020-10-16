ea56630604874d19323a02cc31b226aa45cf70fe
Vinoth Kumar <vinothg.cdm@gmail.com>
2020-10-16 16:44:09 +0800

    Abandoned lock demo

---
+++ parallel-prog/12-abandoned-lock-demo/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * Two philosophers, thinking and eating sushi
+ */
+#include <thread>
+#include <mutex>
+
+int sushi_count = 5000;
+
+void philosopher(std::mutex &chopsticks)
+{
+    while (sushi_count > 0) {
+        std::scoped_lock lock(chopsticks);
+        // chopsticks.lock();
+        if (sushi_count) {
+            sushi_count--;
+        }
+        if (sushi_count ==10) {
+            break;
+        }
+        // chopsticks.unlock();
+    }
+}
+
+int main()
+{
+    std::mutex chopsticks;
+    std::thread barron(philosopher, std::ref(chopsticks));
+    std::thread olivia(philosopher, std::ref(chopsticks));
+    barron.join();
+    olivia.join();
+    printf("The philosophers are done eating.\n");
+}
